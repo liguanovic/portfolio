7539d1fcaea798d4607887e33624f0a61a010f8b
liguanovic <liguanovic787878@gmail.com>
2024-10-10 15:05:20 +0400

    add loader

--- src/Page/Home/home.jsx
+++ src/Page/Home/home.jsx
@@ -10,6 +10,15 @@ import './home.css';
 const Home = () => {
     return (
         <main id="home">
+
+            <div class="loader">
+                <div class="loader__items">
+                    <div class="loader__item loader__item--1"></div>
+                    <div class="loader__item loader__item--2"></div>
+                    <div class="loader__item loader__item--3"></div>
+                </div>
+            </div>
+
             <About />
             <Skills competences={Data.competences} />
             <Projects />
